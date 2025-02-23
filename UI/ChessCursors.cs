6bf2c1c6503bed0472ce6fa139fbcc3683779cbc
Tusk <p3dvvr@gmail.com>
2025-02-23 08:29:37 -0600

    make game interactable

---
+++ UI/ChessCursors.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UI
+{
+    public static class ChessCursors
+    {
+        public static readonly Cursor WhiteCursor = LoadCursor("assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("assets/CursorB.cur");
+        private static Cursor LoadCursor(string filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, UriKind.Relative)).Stream;
+            return new Cursor(stream, true);
+        }
+    }
+}
